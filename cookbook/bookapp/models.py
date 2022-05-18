5c73c0c9c3718b977897cb87d354b18f7d4e7fa8
Mary True <mat-vsuwt@ya.ru>
2022-05-18 09:52:44 +0300

    Что я люблю больше: пожрать или кодить?

--- cookbook/bookapp/models.py
+++ cookbook/bookapp/models.py
@@ -1,41 +1,59 @@
 from django.db import models
 
 # Create your models here.
+class TimeStamp(models.Model):
+    """
+    Abstract - для нее не создаются новые таблицы
+    данные хранятся в каждом наследнике
+    """
+    create = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
 
-class Category(models.Model):
-    # Id не надо, он уже сам появится
-    name = models.CharField(max_length=16, unique=True)
+    class Meta:
+        abstract = True
+
+class Mother(models.Model):
+    """
+    Abstract - для нее не создаются новые таблицы
+    данные хранятся в каждом наследнике
+    """
+    name = models.CharField(max_length=32, unique=True)
     description = models.TextField(blank=True)
 
-    def __str__(self):
-        return self.name
-
-class Difficulty(models.Model):
-
-    name = models.CharField(max_length=16, unique=True)
+    class Meta:
+        abstract = True
+class Category(TimeStamp, Mother):
+    # Id не надо, он уже сам появится
 
 
     def __str__(self):
         return self.name
 
-class Ingredients_group(models.Model):
+class Difficulty(Mother):
 
-    name = models.CharField(max_length=16, unique=True)
+
 
 
     def __str__(self):
         return self.name
 
-class Ingredient(models.Model):
+class Ingredients_group(Mother):
 
-    name = models.CharField(max_length=16, unique=True)
+
+
+
+    def __str__(self):
+        return self.name
+
+class Ingredient(Mother):
+
     group = models.ForeignKey(Ingredients_group, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
 
-class Recipes(models.Model):
-    name = models.CharField(max_length=16, unique=True)
+class Recipes(TimeStamp, Mother):
+
     picture = models.ImageField(upload_to='posts', null=True, blank=True)
     ingredients = models.ManyToManyField(Ingredient)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
--- cookbook/bookapp/views.py
+++ cookbook/bookapp/views.py
@@ -1,19 +1,19 @@
 from django.shortcuts import render, get_object_or_404, HttpResponseRedirect
-from django.urls import reverse
-
-from .models import Recipes
-from .forms import ContactForm
+from django.urls import reverse, reverse_lazy
+from .models import Ingredient
+from .models import Recipes, Category, Ingredients_group
+from .forms import ContactForm, PostForm
 from django.core.mail import send_mail
-
-
+from django.views.generic.base import ContextMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django import forms
 # Create your views here.
-def main_view(request):
-    posts = Recipes.objects.all()
-    return render(request, 'bookapp/index.html', context={'posts': posts})
+# def main_view(request):
+#     posts = Recipes.objects.all()
+#     return render(request, 'bookapp/index.html', context={'posts': posts})
 
 
-
-def create_recipe(request):
+def contact(request):
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
@@ -30,13 +30,188 @@ def create_recipe(request):
                 fail_silently=True,
             )
 
-            return HttpResponseRedirect(reverse('blog:index'))
+            return HttpResponseRedirect(reverse('cookbook:index'))
         else:
-            return render(request, 'bookapp/create.html', context={'form': form})
+            return render(request, 'bookapp/contact.html', context={'form': form})
     else:
         form = ContactForm()
-        return render(request, 'bookapp/create.html', context={'form': form})
+        return render(request, 'bookapp/contact.html', context={'form': form})
 
 def post(request, id):
     post = get_object_or_404(Recipes, id=id)
     return render(request, 'bookapp/post.html', context={'post': post})
+
+def create_recipe(request):
+    if request.method == 'GET':
+        form = PostForm()
+        return render(request, 'bookapp/create.html', context={'form': form})
+    else:
+        form = PostForm(request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('cookbook:index'))
+        else:
+            return render(request, 'bookapp/create.html', context={'form': form})
+
+class NameContextMixin(ContextMixin):
+
+    def get_context_data(self, *args, **kwargs):
+        """
+        Отвечает за передачу параметров в контекст
+        :param args:
+        :param kwargs:
+        :return:
+        """
+        context = super().get_context_data(*args, **kwargs)
+        context['name'] = 'Продукты:'
+        return context
+# CRUD CREATE, READ (LIST, DETAIL), UPDATE, DELETE
+# список тегов
+class IngredientListView(ListView, NameContextMixin):
+    model = Ingredient
+    template_name = 'bookapp/ingredient_list.html'
+    context_object_name = 'Ingredient'
+
+
+
+class IngredientFilterListView(ListView, NameContextMixin):
+    model = Ingredient
+    template_name = 'bookapp/ingredient_list.html'
+    context_object_name = 'Ingredient'
+
+    def get_queryset(self):
+
+        self.group = get_object_or_404(Ingredients_group, id=self.kwargs['pk'])
+        return Ingredient.objects.filter(group=self.group)
+
+
+
+
+# детальная информация
+class IngredientDetailView(DetailView, NameContextMixin):
+    fields = '__all__'
+    model = Ingredient
+    template_name = 'bookapp/ingredient_detail.html'
+
+    def get(self, request, *args, **kwargs):
+        """
+        Метод обработки get запроса
+        :param request:
+        :param args:
+        :param kwargs:
+        :return:
+        """
+        self.tag_id = kwargs['pk']
+        return super().get(request, *args, **kwargs)
+
+    def get_object(self, queryset=None):
+        """
+        Получение этого объекта
+        :param queryset:
+        :return:
+        """
+        return get_object_or_404(Ingredient, pk=self.tag_id)
+
+
+# создание тега
+class IngredientCreateView(CreateView, NameContextMixin):
+    # form_class =
+    fields = '__all__'
+    model = Ingredient
+    success_url = reverse_lazy('cookbook:ingredient_list')
+    template_name = 'bookapp/ingredient_create.html'
+
+    def post(self, request, *args, **kwargs):
+        """
+        Пришел пост запрос
+        :param request:
+        :param args:
+        :param kwargs:
+        :return:
+        """
+        return super().post(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        """
+        Метод срабатывает после того как форма валидна
+        :param form:
+        :return:
+        """
+        return super().form_valid(form)
+
+
+class IngredientUpdateView(UpdateView):
+    fields = '__all__'
+    model = Ingredient
+    success_url = reverse_lazy('cookbook:ingredient_list')
+    template_name = 'bookapp/ingredient_create.html'
+
+
+class IngredientDeleteView(DeleteView):
+    template_name = 'bookapp/ingredient_delete.html'
+    model = Ingredient
+    success_url = reverse_lazy('cookbook:ingredient_list')
+
+class GroupListView(ListView, NameContextMixin):
+    model = Ingredients_group
+    template_name = 'bookapp/ingredient_group.html'
+    context_object_name = 'Group'
+
+    # def get_queryset(self):
+    #     """
+    #     Получение данных
+    #     :return:
+    #     """
+    #     return Ingredient.objects.filter()
+
+
+class MainView(ListView):
+    model = Recipes
+    template_name = 'bookapp/main.html'
+    context_object_name = 'Recipes'
+
+
+class RecipeCreateView(CreateView):
+    # form_class =
+    fields = '__all__'
+    model = Recipes
+    success_url = reverse_lazy('cookbook:index')
+    template_name = 'bookapp/recipe_create.html'
+
+class RecipeDetailView(DetailView):
+    fields = '__all__'
+    model = Recipes
+    tags = forms.ModelMultipleChoiceField(queryset=Ingredient.objects.all(),
+                                          widget=forms.CheckboxSelectMultiple())
+    template_name = 'bookapp/recipe_detail.html'
+
+    def get(self, request, *args, **kwargs):
+        """
+        Метод обработки get запроса
+        :param request:
+        :param args:
+        :param kwargs:
+        :return:
+        """
+        self.tag_id = kwargs['pk']
+        return super().get(request, *args, **kwargs)
+
+    def get_object(self, queryset=None):
+        """
+        Получение этого объекта
+        :param queryset:
+        :return:
+        """
+        return get_object_or_404(Recipes, pk=self.tag_id)
+
+class RecipeUpdateView(UpdateView):
+    fields = '__all__'
+    model = Recipes
+    success_url = reverse_lazy('cookbook:index')
+    template_name = 'bookapp/recipe_create.html'
+
+
+class RecipeDeleteView(DeleteView):
+    template_name = 'bookapp/ingredient_delete.html'
+    model = Recipes
+    success_url = reverse_lazy('cookbook:index')
